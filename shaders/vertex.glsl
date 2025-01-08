50f733ec69669faeaf3d185a2988c6e34affacca
Dethcrvsh <fabianfabbe@hotmail.se>
2025-01-09 00:44:01 +0100

    Shader test

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,6 +1,9 @@
 #version 330 core
-layout(location = 0) in vec3 position;
+
+in vec3 position;
+out vec3 vertex_color;
 
 void main() {
     gl_Position = vec4(position, 1.0);
+    vertex_color = position;
 }
